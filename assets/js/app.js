556243bf45faff1c82f577c4a4697aa70cba9ad2
parth vashisth <shrmaparth123@gmail.com>
2024-04-27 14:55:59 +0530

    slick-sliders done

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+$(".parentslider").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    // autoplay: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false
+            }
+        }
+    ]
+});
